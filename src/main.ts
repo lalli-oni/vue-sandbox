aa698a77636509e760e887701bdf577ac453422d
lalli-oni <lalli.oni@gmail.com>
2024-07-15 14:29:46 +0200

    Initialize service workers

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+ 
+  const { worker } = await import('./mocks/browser')
+ 
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start()
+}
+
+enableMocking()
+
 createApp(App).mount('#app')
