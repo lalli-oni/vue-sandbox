1d02963288c68e764bd6ad4cd4ecff1a63f893d4
lalli-oni <lalli.oni@gmail.com>
2024-07-15 14:14:11 +0200

    Import resights test case

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import axios from "axios";
+
+const pageTitle = "Data Fetching Example";
+const isLoading = ref(false);
+const data = ref([]);
+
+const fetchData = async () => {
+  try {
+    isLoading.value = true;
+    const response = await axios.get("https://api.example.com/data");
+    data = response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  } finally {
+    isLoading.value = false;
+  }
+};
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>{{ pageTitle }}</h1>
+    <button @click="fetchData">Fetch Data</button>
+    <div v-if="isLoading">Loading...</div>
+    <ul>
+      <li v-for="item in data" :key="item.id">{{ item.name }}</li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
